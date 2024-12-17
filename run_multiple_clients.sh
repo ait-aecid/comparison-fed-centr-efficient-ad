59677c28dfde9880f991e3a1f7e77de76e4271d6
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2024-12-17 15:27:01 +0100

    ADD script to run multiple clients at once: run_multiple_clients.sh

---
+++ run_multiple_clients.sh
@@ -0,0 +1,13 @@
+
+# Arguments run
+RUN_NUMBER=0
+CONFIG_PATH="config_files/bgl_no_iid.yaml"
+NUM_CLIENTS=5
+METHODS="KnowEvents 3-Gram"
+
+ITER=$(($NUM_CLIENTS - 1))
+for i in $(seq 0 $ITER); do 
+    echo Starting client number: $i
+    echo "python client_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER --num_client $i"
+    python client_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER --num_client $i &
+done
