cdcd1004d98e787ca3895a4615859486bc48351a
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2025-02-27 11:18:44 +0100

    ADD now run_multiple_clients can change the variables with arguments

--- run_multiple_clients.sh
+++ run_multiple_clients.sh
@@ -1,9 +1,22 @@
+# Default arguments
+DEFAULT_RUN_NUMBER=2
+DEFAULT_CONFIG_PATH="config_files/bgl_iid.yaml"
+DEFAULT_NUM_CLIENTS=5
+DEFAULT_METHODS="2-Gram LengthDetection"
 
-# Arguments run
-RUN_NUMBER=2
-CONFIG_PATH="config_files/bgl_iid.yaml"
-NUM_CLIENTS=5
-METHODS="2-Gram LengthDetection"
+
+# Getting final arguments
+RUN_NUMBER=${1:-$DEFAULT_RUN_NUMBER}
+CONFIG_PATH=${2:-$DEFAULT_CONFIG_PATH}
+NUM_CLIENTS=${3:-$DEFAULT_NUM_CLIENTS}
+METHODS=${4:-$DEFAULT_METHODS}
+
+echo "Variables:"
+echo "      - RUN_NUMBER: $RUN_NUMBER"
+echo "      - CONFIG_PATH: $CONFIG_PATH"
+echo "      - NUM_CLIENTS: $NUM_CLIENTS"
+echo "      - METHODS: $METHODS"
+sleep 2
 
 
 # Run main server
@@ -11,6 +24,7 @@ echo "Start Server"
 echo "python server_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER --amount_clients $NUM_CLIENTS"
 python server_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER  --amount_clients $NUM_CLIENTS&
 sleep 5
+
 
 # Run all the clients
 echo "Start Clients"
@@ -20,6 +34,7 @@ for i in $(seq 0 $ITER); do
     echo "python client_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER --num_client $i --amount_clients $NUM_CLIENTS"
     python client_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER --num_client $i  --amount_clients $NUM_CLIENTS&
 done
+
 
 # Wait until finish
 wait
