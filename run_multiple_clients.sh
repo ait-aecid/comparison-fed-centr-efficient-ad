d019fb9762408a1ddbb38a48437c51fc970bedf8
angre.garcia-gomez@ait.ac.at <angre.garcia-gomez@ait.ac.at>
2025-02-27 09:33:40 +0100

    ADD now in run_multiple_clients also starts the main server

--- run_multiple_clients.sh
+++ run_multiple_clients.sh
@@ -1,13 +1,25 @@
 
 # Arguments run
-RUN_NUMBER=0
-CONFIG_PATH="config_files/bgl_no_iid.yaml"
-NUM_CLIENTS=10
-METHODS="KnowEvents"
+RUN_NUMBER=2
+CONFIG_PATH="config_files/bgl_iid.yaml"
+NUM_CLIENTS=5
+METHODS="2-Gram LengthDetection"
 
+# Run main server
+echo "Start Server"
+echo "python server_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER"
+python server_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER &
+sleep 5
+
+# Run all the clients
+echo "Start Clients"
 ITER=$(($NUM_CLIENTS - 1))
 for i in $(seq 0 $ITER); do 
     echo Starting client number: $i
     echo "python client_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER --num_client $i"
     python client_app.py --config $CONFIG_PATH --method $METHODS --run_number $RUN_NUMBER --num_client $i &
 done
+
+# Wait until finish
+wait
+echo "All jobs are finish"
